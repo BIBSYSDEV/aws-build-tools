f976dc5bff55080e1a57f4bfb3611724185712ef
Orestis Gkorgkas <og@bibsys.no>
2018-11-27 13:15:30 +0100

    feat: move from aut-reg-inf

---
+++ src/main/java/no/bibsys/aws/apigateway/ApiGatewayConstants.java
@@ -0,0 +1,7 @@
+package no.bibsys.aws.apigateway;
+
+public class ApiGatewayConstants {
+
+    public static final String OPEN_API_3 = "oas30";
+
+}
---
+++ src/main/java/no/bibsys/aws/tools/Environment.java
@@ -0,0 +1,25 @@
+package no.bibsys.aws.tools;
+
+import com.google.common.base.Preconditions;
+import java.util.Optional;
+
+public class Environment {
+
+
+
+    public Optional<String> readEnvOpt(String variableName) {
+        return Optional.ofNullable(System.getenv().get(variableName)).filter(value -> !value.isEmpty());
+
+
+    }
+
+
+    public String readEnv(String variableName) {
+        String value = System.getenv().get(variableName);
+        Preconditions.checkNotNull(value, variableName + " env variable was not found");
+        return value;
+
+    }
+
+
+}
