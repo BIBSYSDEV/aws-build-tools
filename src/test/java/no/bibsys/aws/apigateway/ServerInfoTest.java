0d8c66d028d8f2672c594dc7b5cf9094939d8b1b
Orestis Gkorgkas <og@bibsys.no>
2019-02-04 11:31:09 +0100

    reformatting: magic strings and typos

--- src/test/java/no/bibsys/aws/apigateway/ServerInfoTest.java
+++ src/test/java/no/bibsys/aws/apigateway/ServerInfoTest.java
@@ -8,17 +8,22 @@ import org.junit.jupiter.api.Test;
 
 public class ServerInfoTest {
 
+    private static final String BASE_PATH = "{basePath}";
+    private static final String HTTPS_SAMPLE_API_GATEWAY_URL =
+        "https://xxxxxxxx.execute-api.eu-west-1.amazonaws.com/" + BASE_PATH;
+    private static final String HTTP_SAMPLE_API_GATEWAY_URL =
+        "http://xxxxxxxx.execute-api.eu-west-1.amazonaws.com/" + BASE_PATH;
+    private static final String FINAL_STAGE = "final";
+
     @Test
-    public void completeServerUrl_HttpsserverUrlAndStage_workingSeverUrl() {
-        ServerInfo serverInfo = new ServerInfo("https://xxxxxxxx.execute-api.eu-west-1.amazonaws.com/{basePath}",
-            "final");
-        assertThat(serverInfo.serverAddress(), is(equalTo("xxxxxxxx.execute-api.eu-west-1.amazonaws.com")));
+    public void completeServerUrl_HttpsServerUrlAndStage_workingServerUrl() {
+        ServerInfo serverInfo = new ServerInfo(HTTPS_SAMPLE_API_GATEWAY_URL + BASE_PATH, FINAL_STAGE);
+        assertThat(serverInfo.serverAddress(), is(equalTo(HTTPS_SAMPLE_API_GATEWAY_URL)));
     }
 
     @Test
-    public void completeServerUrl_HttpserverUrlAndStage_workingSeverUrl() {
-        ServerInfo serverInfo = new ServerInfo("http://xxxxxxxx.execute-api.eu-west-1.amazonaws.com/{basePath}",
-            "final");
-        assertThat(serverInfo.serverAddress(), is(equalTo("xxxxxxxx.execute-api.eu-west-1.amazonaws.com")));
+    public void completeServerUrl_HttpServerUrlAndStage_workingServerUrl() {
+        ServerInfo serverInfo = new ServerInfo(HTTP_SAMPLE_API_GATEWAY_URL + BASE_PATH, FINAL_STAGE);
+        assertThat(serverInfo.serverAddress(), is(equalTo(HTTP_SAMPLE_API_GATEWAY_URL)));
     }
 }
