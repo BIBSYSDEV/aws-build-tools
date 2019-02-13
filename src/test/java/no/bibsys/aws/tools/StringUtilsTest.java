0b79048c8317fc713f4e0ed70d95195828b67d99
Orestis Gkorgkas <og@bibsys.no>
2019-02-13 15:34:34 +0100

    AUTREG-162: test coverage

--- src/test/java/no/bibsys/aws/tools/StringUtilsTest.java
+++ src/test/java/no/bibsys/aws/tools/StringUtilsTest.java
@@ -1,5 +1,9 @@
 package no.bibsys.aws.tools;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.regex.Matcher;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.lessThan;
 import static org.hamcrest.core.Is.is;
@@ -7,11 +11,12 @@ import static org.hamcrest.core.IsEqual.equalTo;
 import static org.hamcrest.core.IsNot.not;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.util.regex.Matcher;
-import org.junit.jupiter.api.Test;
-
 public class StringUtilsTest extends AmazonNamingRestrictions {
-
+    
+    private static final int SMALL_LENGTH = 2;
+    private static final int BIG_LENGTH = 10;
+    private static final StringUtils stringUtils = new StringUtils();
+    
     @Test
     public void normalizedStringsShoudComplyWithAmazonConstraints() {
         String branchName = "AUTREG-131_ensure_javascript_linting_is_in_place";
@@ -19,7 +24,7 @@ public class StringUtilsTest extends AmazonNamingRestrictions {
         Matcher matcher = amazonPattern.matcher(normalized);
         assertThat(matcher.matches(), is(equalTo(true)));
     }
-
+    
     @Test
     public void randomString_smallMaxLength_randomStringOfLengthMaxLength() {
         int smallLength = 5;
@@ -29,7 +34,7 @@ public class StringUtilsTest extends AmazonNamingRestrictions {
         assertThat(randomString2.length(), is(equalTo(smallLength)));
         assertThat(randomString1, is(not(equalTo(randomString2))));
     }
-
+    
     @Test
     public void randomString_largeMaxLength_randomStringOfLengthNotGreaterThanMaxLength() {
         int largeLegnth = 3000;
@@ -39,9 +44,19 @@ public class StringUtilsTest extends AmazonNamingRestrictions {
         assertThat(randomString2.length(), is(lessThan(largeLegnth)));
         assertThat(randomString1, is(not(equalTo(randomString2))));
     }
-
+    
     @Test
     public void randomString_zeroLength_throwsException() {
         assertThrows(IllegalArgumentException.class, () -> stringUtils.randomString(0));
+    }
+    
+    @Test
+    public void shortString_stringNonZeroLength_stringWithGivenLength() {
+        String test = "ABDCEFG";
+        String short1 = stringUtils.shortNormalizedString(test, SMALL_LENGTH);
+        assertThat(short1.length(), is(equalTo(2)));
+        
+        String short2 = stringUtils.shortNormalizedString(test, BIG_LENGTH);
+        assertThat(short2.length(), is(equalTo(test.length())));
     }
 }
