f2dc763e28b93e943df9d7d689a903a3cb659f7c
Orestis Gkorgkas <og@bibsys.no>
2019-02-15 11:45:21 +0100

    AUTREG-162: spaces

--- src/main/java/no/bibsys/aws/secrets/SecretsReader.java
+++ src/main/java/no/bibsys/aws/secrets/SecretsReader.java
@@ -3,6 +3,6 @@ package no.bibsys.aws.secrets;
 import java.io.IOException;
 
 public interface SecretsReader {
-     
-     String readSecret() throws IOException;
+    
+    String readSecret() throws IOException;
 }
