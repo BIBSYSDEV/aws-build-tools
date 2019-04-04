5530c4eb9199a3f19c9a1f0515422d69f0ffe43b
Orestis Gkorgkas <og@bibsys.no>
2019-04-04 10:08:07 +0200

    AUTREG-177: The simplest PipelineLambdaFunction

---
+++ src/test/java/no/bibsys/aws/lambda/handlers/templates/SimplePipelineLambdaFunctionTest.java
@@ -0,0 +1,29 @@
+package no.bibsys.aws.lambda.handlers.templates;
+
+import no.bibsys.aws.lambda.handlers.LocalTest;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsNot.not;
+
+class SimplePipelineLambdaFunctionTest extends LocalTest {
+    
+    private final transient String inputObject = "sample";
+    private SimplePipelineLambdaFunction function;
+    
+    public SimplePipelineLambdaFunctionTest() throws IOException {
+        super();
+        CodePipelineCommunicator communicator = new MockCodePipelineCommunicator();
+        function = new SimplePipelineLambdaFunction(communicator);
+    }
+    
+    @Test
+    public void processInputShouldReturnAnNonNullOutput() {
+        Object output = function.processInput(inputObject, inputObject, null);
+        assertThat(output, is(not(equalTo(null))));
+    }
+}
